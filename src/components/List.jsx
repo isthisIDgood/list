66de51eca6ec32a75d515679e6cb66caa940299a
Brent Nolan <brentnolan@Brents-MacBook-Pro.local>
2020-02-23 16:13:56 -0600

    renders

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { ToDo } from './ToDo';
 
-export function List() {
-    const todos = [
-        "thing 1",
-        "thing 2",
-        "thing 3",
-        "thing 4",
-        "thing 5",
-        "thing 6",
-    ];
+export function List({ todos = [
+    "thing 1",
+    "thing 2",
+    "thing 3",
+    "thing 4",
+    "thing 5",
+    "thing 6",
+    "thing 7",
+] }) {
+    
+    function renderTodos() {
+        return todos.map((todo, i) => <ToDo key={todo} task = {todo} />);
+    }
 
-    const todosToRender = []
     return (
         <>
             {/* TODO: make a list of <ToDo /> */}
-            <ul>{todosToRender}</ul>
+            <ul>
+                {renderTodos()}
+            </ul>    
             <button>add a todo</button>
         </>
     )
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export function ToDo({ task }) {
+    console.log('todo: ', task);
     return <li>{task}</li>
 }
